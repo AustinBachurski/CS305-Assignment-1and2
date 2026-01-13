05d3984606cc14caab7c681c3c61ea49fcfb6244
Austin Bachurski <austinbachurski@gmail.com>
2026-01-13 15:24:27 -0700

    Add parsing for input values.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,49 @@
-void demo(); // Forward declaration for the `demo` function.
+#include "process.hpp"
+#include "schedulingAlgorithms.hpp"
 
-int main()   // Program entry point.
+#include <charconv>
+#include <concepts>
+#include <cstring>
+#include <functional>
+#include <print>
+
+
+void demo(std::function<unsigned(Process const, unsigned const)>, unsigned const serviceTime); // Forward declaration for the `demo` function.
+
+template<std::unsigned_integral T>
+bool argvIsNumber(std::string_view argv, T& valueOut)
 {
-    demo();  // Call the demo function.
+    return !(std::from_chars(argv.data(), argv.data() + argv.size(), valueOut).ec == std::errc{});
+}
+
+void printUsage()
+{
+    std::println("Usage: assignment1 <--algorithm> <service_time> (i.e. assignment1 --RMS 55)\nAvailable algorithms:\n\t\t--RMS\n\t\t--DMS\n\t\t--EDF");
+}
+
+int main(int argc, char **argv)   // Program entry point.
+{
+    unsigned serviceTime{};
+
+    if (argc < 3 || argvIsNumber(argv[2], serviceTime))
+    {
+        printUsage();
+    }
+    else if (std::strcmp(argv[1], "--RMS") == 0)
+    {
+        demo(SchedulingAlgorithm::rateMonotonic, serviceTime);
+    }
+    else if (std::strcmp(argv[1], "--DMS") == 0)
+    {
+        demo(SchedulingAlgorithm::deadlineMonotonic, serviceTime);
+    }
+    else if (std::strcmp(argv[1], "--EDF") == 0)
+    {
+        demo(SchedulingAlgorithm::earliestDeadlineFirst, serviceTime);
+    }
+    else
+    {
+        printUsage();
+    }
 }
 
