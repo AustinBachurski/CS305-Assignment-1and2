bba0872b03b2c4a4171c15053fceb236d2f0aa13
Austin Bachurski <austinbachurski@gmail.com>
2026-01-13 15:26:53 -0700

    Add scheduler functionality.

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -1,5 +1,84 @@
 #include "scheduler.hpp"
 #include "process.hpp"
 
+#include <functional>
+#include <print>
 
+
+Scheduler::Scheduler(SchedulingAlgorithm schedulingAlgorithm, unsigned &systemClock)
+: algorithm{ schedulingAlgorithm }
+, clock{ systemClock }
+{};
+
+void Scheduler::queueProcess(Process process)
+{
+    process.priority = std::invoke(algorithm, process, clock);
+    process.relativeDeadline = clock + process.relativeDeadline;
+
+    if (!currentProcess.processID)
+    {
+        currentProcess = process;
+    }
+    else if (process.priority < currentProcess.priority)
+    {
+        processQueue.push(currentProcess);
+        currentProcess = process;
+    }
+    else
+    {
+        processQueue.push(process);
+    }
+}
+
+void Scheduler::report()
+{
+    std::println("Schedule is feasible from 0 to {} when process {} did not meet deadline.", clock, currentProcess.processID);
+
+
+}
+
+void Scheduler::setServiceTime(unsigned const time)
+{
+    serviceTime = time;
+}
+
+bool Scheduler::tick()
+{
+    // Increment the current clock and the run time tracking for the current process.
+    // If processID is zero, we're tracking idle time.
+    ++clock;
+    ++tracking[currentProcess.processID].executionTime;
+
+    // CPU is idle.
+    if (!currentProcess.processID)
+    {
+        return true;
+    }
+
+    // Process missed deadline.
+    if (currentProcess.relativeDeadline < clock)
+    {
+        return false;
+    }
+
+
+    // Process complete.
+    if (tracking[currentProcess.processID].executionTime == serviceTime)
+    {
+        tracking[currentProcess.processID].completionTimes.push_back(clock);
+
+        if (!processQueue.empty())
+        {
+            currentProcess = processQueue.top();
+            processQueue.pop();
+        }
+        else
+        {
+            currentProcess = {};
+        }
+    }
+
+    // Current process is either running, or CPU is idling.
+    return true;
+}
 
--- src/scheduler.hpp
+++ src/scheduler.hpp
@@ -6,24 +6,27 @@
 #include <deque>
 #include <functional>
 #include <queue>
+#include <unordered_map>
 
 
 // Scheduler class that manages scheduling CPU time.
 class Scheduler
 {
 public:
-    using SchedulingAlgorithm = std::function<uint8_t(Process const)>;
-    explicit Scheduler(SchedulingAlgorithm schedulingAlgorithm)
-    : algorithm{ schedulingAlgorithm }
-    {};
-    void queueProcess(Process const);
+    using SchedulingAlgorithm = std::function<unsigned(Process const, unsigned const)>;
+    explicit Scheduler(SchedulingAlgorithm schedulingAlgorithm, unsigned &clock);
+    void queueProcess(Process process);
+    void report();
+    void setServiceTime(unsigned const time);
+    bool tick();
 
 private:
-    Process updatePriority(Process process) const;
-
     SchedulingAlgorithm algorithm;
+    unsigned &clock;
     Process currentProcess{};
-    std::priority_queue<Process, std::deque<Process>, ProcessComparitor> queue;
+    std::priority_queue<Process, std::deque<Process>, ProcessComparitor> processQueue;
+    unsigned serviceTime{};
+    std::unordered_map<unsigned, ProcessData> tracking;
 };
 
 #endif
