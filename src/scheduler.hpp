44b75752db82a99979902338119158d8cebb00cf
Austin Bachurski <austinbachurski@gmail.com>
2026-01-13 09:33:01 -0700

    Take function object in the constructor.

--- src/scheduler.hpp
+++ src/scheduler.hpp
@@ -1,12 +1,29 @@
 #ifndef SCHEDULER_HPP
 #define SCHEDULER_HPP
 
+#include "process.hpp"
+
+#include <deque>
+#include <functional>
+#include <queue>
+
 
 // Scheduler class that manages scheduling CPU time.
 class Scheduler
 {
 public:
+    using SchedulingAlgorithm = std::function<uint8_t(Process const)>;
+    explicit Scheduler(SchedulingAlgorithm schedulingAlgorithm)
+    : algorithm{ schedulingAlgorithm }
+    {};
+    void queueProcess(Process const);
+
 private:
+    Process updatePriority(Process process) const;
+
+    SchedulingAlgorithm algorithm;
+    Process currentProcess{};
+    std::priority_queue<Process, std::deque<Process>, ProcessComparitor> queue;
 };
 
 #endif
