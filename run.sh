316b8b510fd3a3d32055bdd96a3c1ca2b5a76f6a
Austin Bachurski <austinbachurski@gmail.com>
2026-01-14 10:25:49 -0700

    Update run script to generate output files.

--- run.sh
+++ run.sh
@@ -9,9 +9,10 @@ fi
 cmake --build build
 
 # Run project for each algorithm, outputting to file.
-#./build/bin/assignment2 > output/Program-Output.txt
-./build/bin/assignment3 --RMS 55
+./build/bin/assignment3 --RMS 15 > output/Rate-Monotonic.txt
+./build/bin/assignment3 --DMS 15 > output/Deadline-Monotonic.txt
+./build/bin/assignment3 --EDF 15 > output/Earliest-Deadline-First.txt
 
 # Status code, info dump, and exit.
-#echo "Program exited with code $?.  Output written to 'output' directory."
+echo "Program exited with code $?.  Output written to 'output' directory."
 
